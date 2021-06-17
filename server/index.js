69a45a22067259ddf935eea325df8f98d54ede02
Manu Alba <manualbadj@gmail.com>
2021-06-17 16:39:39 +0200

    final commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const staticFilesPath = path.resolve(__dirname, './public');
 app.use('/', express.static(staticFilesPath));
 
+
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
